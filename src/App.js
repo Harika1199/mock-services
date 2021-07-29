fa192e7722fe249e316ab665fdcb00fce11a3ae4
Harika <harika3172@gmail.com>
2021-07-29 23:37:00 +0530

    basic html and css is done

--- src/App.js
+++ src/App.js
@@ -4,20 +4,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="section1">
+        <div className="header">
+          <h1>MOCK SERVICES</h1>
+        </div>
+        <div className="add-input">
+          <input type="url" placeholder="Enter URL" name="url"  autoFocus={true} required/>
+          <button>ADD</button>
+        </div>
+      </div>
+      <div className="section2">
+        <div className="url-list">
+          <h2>Saved Urls</h2>
+          <div className="list-div">
+            <ul>
+              <li>http://youtube.com</li>
+              <li>http://google.com</li>
+              <li>http://linkedin.com</li>
+              <li>http://facebook.com</li>
+            </ul>
+          </div>
+        </div>
+        <div className="response">
+          <h2 className="response-heading">Response</h2>
+          <div className="response-text">
+            <p>Response will be displayed here</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
